fbac1fe5edd290c329fa7f01dd0fb10543edb7fd
gale <gale.a.chapman@gmail.com>
2018-10-15 22:19:49 -0400

    name updates

--- src/Wk2Lab7Validator/Wk2Lab7Validator.java
+++ src/Wk2Lab7Validator/Wk2Lab7Validator.java
@@ -10,7 +10,7 @@ public class Wk2Lab7Validator {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 	
-		System.out.println(Validator.matchRegex(scan, "Enter your name: ", "[A-Z][a-z]{2,30}"));
+		System.out.println(Validator.matchRegex(scan, "Enter your name: ", "\\p{Alpha}{2,30}"));
 		
 		System.out.println(Validator.matchRegex(scan, "Enter your email: ", "\\w+{5,30}@\\w+{5,10}\\.\\w{2,3}"));
 		
